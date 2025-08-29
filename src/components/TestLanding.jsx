7cab2e7ee2eee87cb88f4d8ba0b6a9d3197b1f85
Reagz24 <reagang23@gmail.com>
2025-08-29 15:17:30 -0400

    Add test landing page to debug routing

---
+++ src/components/TestLanding.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TestLanding = () => {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1 style={{ color: 'red', fontSize: '48px' }}>TEST LANDING PAGE</h1>
+      <p style={{ fontSize: '24px' }}>This is a test page to see if routing works.</p>
+      <p>If you can see this, the routing is working!</p>
+    </div>
+  );
+};
+
+export default TestLanding;
