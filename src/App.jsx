3fa39328a5eba053fc022da994d01e035790d5d1
Reagz24 <reagang23@gmail.com>
2025-08-29 15:56:59 -0400

    Add test landing page to debug routing

--- src/App.jsx
+++ src/App.jsx
@@ -43,6 +43,7 @@ import house6 from './assets/house6.svg';
 import NCLanding from "./components/NCLanding.jsx";
 import WilmingtonLanding from "./components/WilmingtonLanding.jsx";
 import SimpleLanding from "./components/SimpleLanding.jsx";
+import TestLanding from "./components/TestLanding.jsx";
 
 const allReviews = [
   {
@@ -716,6 +717,7 @@ function App() {
         <Route path="/wilmington-landing" element={<WilmingtonLanding />} />
         <Route path="/nc-landing" element={<NCLanding />} />
         <Route path="/new-landing" element={<SimpleLanding />} />
+        <Route path="/test-landing" element={<TestLanding />} />
       </Routes>
     </Router>
   );
--- src/components/TestLanding.jsx
+++ src/components/TestLanding.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 
 const TestLanding = () => {
   return (
-    <div style={{ padding: '50px', textAlign: 'center' }}>
-      <h1 style={{ color: 'red', fontSize: '48px' }}>TEST LANDING PAGE</h1>
-      <p style={{ fontSize: '24px' }}>This is a test page to see if routing works.</p>
-      <p>If you can see this, the routing is working!</p>
+    <div style={{ 
+      color: 'red', 
+      fontSize: '3em', 
+      textAlign: 'center', 
+      marginTop: '100px',
+      padding: '20px',
+      backgroundColor: 'yellow'
+    }}>
+      TEST LANDING PAGE - IF YOU SEE THIS, ROUTING WORKS!
     </div>
   );
 };
