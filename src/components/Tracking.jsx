e724c2afe6f6aae5adb08f2178878628a3ef049a
Reagz24 <reagang23@gmail.com>
2025-09-01 13:01:47 -0400

    Fix Facebook pixel duplication: prevent double initialization in Tracking.jsx

--- src/components/Tracking.jsx
+++ src/components/Tracking.jsx
@@ -28,19 +28,22 @@ const Tracking = () => {
         `}
       </script>
 
-      {/* Meta Pixel */}
+      {/* Meta Pixel - Only initialize if not already done */}
       <script>
         {`
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '${META_PIXEL_ID}');
-          fbq('track', 'PageView');
+          if (!window.fbq && !window._fbPixelInitialized) {
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${META_PIXEL_ID}');
+            fbq('track', 'PageView');
+            window._fbPixelInitialized = true;
+          }
         `}
       </script>
 
@@ -155,4 +158,3 @@ export const trackBlogScroll = (blogTitle) => {
 };
 
 export default Tracking;
-
